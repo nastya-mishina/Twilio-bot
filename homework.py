966f648ec933dfd333c7be984f583988bd14e8a2
Анастасия <anastasia.petrukhova@gmail.com>
2020-11-25 15:34:56 +0300

    api_sms_my_first_review

--- homework.py
+++ homework.py
@@ -1,40 +1,34 @@
 import time
 import requests
 import os
-
 from twilio.rest import Client
-
 
 def get_status(user_id):
     params = {
         "user_ids": user_id,
         "v": 5.92,
         "fields": "online",
-        "access_token": os.environ["ACCOUNT_TOKEN"],
+        "access_token": os.environ["VK_TOKEN"],
     }
     url = "https://api.vk.com/method/users.get"
     user_info = requests.post(url, params=params)
-    return user_info.json()["response"][0]["online"]  # Верните статус пользователя в ВК
-
+    return user_info.json()["response"][0]["online"]
 
 def sms_sender(sms_text):
     account_sid = os.environ["ACCOUNT_SID"]
-    auth_token = os.environ["TWILIO_ACCOUNT_TOKEN"]
+    auth_token = os.environ["AUTH_TOKEN"]
     client = Client(account_sid, auth_token)
-
     message = client.messages.create(
         body=sms_text,
         from_=os.environ["NUMBER_FROM"],
         to=os.environ["NUMBER_TO"],
     )
-    return message.sid  # Верните sid отправленного сообщения из Twilio
+    return message.sid
 
-
-if __name__ == '__main__':
-    # тут происходит инициализация Client
-    vk_id = input('Введите id ')
+if __name__ == "__main__":
+    vk_id = input("Введите id ")
     while True:
         if get_status(vk_id) == 1:
-            sms_sender(f'{vk_id} сейчас онлайн!')
+            sms_sender(f"{vk_id} сейчас онлайн!")
             break
         time.sleep(5)
